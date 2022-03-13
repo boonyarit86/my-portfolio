0010932fabcd751960975b565bd9b561a7c8d03b
Boonyarit Ritdet <boonyaritza93@hotmail.com>
2022-03-13 18:21:25 +0700

    Edit Nav Style

--- src/App.js
+++ src/App.js
@@ -6,35 +6,34 @@ import Header from "./Layouts/Header";
 import Main from "./Layouts/Main";
 
 function App() {
-  // useEffect(() => {
-  //   const headerEl = document.querySelector(".header");
-  //   const bgEffectEl = document.querySelector(".header__bg-effect");
+  useEffect(() => {
+    const headerEl = document.querySelector(".header");
+    const navEl = document.querySelector(".nav");
+    
+    const obs = new IntersectionObserver(
+      function (entries) {
+        const ent = entries[0];
+        // console.log(ent);
 
-  //   const obs = new IntersectionObserver(
-  //     function (entries) {
-  //       const ent = entries[0];
-  //       // console.log(ent);
+        if (ent.isIntersecting === false) {
+          console.log("Changed");
+          navEl.style.backgroundColor = "rgba(255, 255, 255, 0.97)";
+        }
 
-  //       if (ent.isIntersecting === false) {
-  //         console.log("Changed");
-  //         bgEffectEl.classList.add("bg-animation");
-  //         bgEffectEl.style.backgroundColor = "#fff"
-  //         // headerEl.style.backgroundColor = "#fff";
-  //       }
-
-  //       if (ent.isIntersecting === true) {
-  //         console.log("normal");
-  //       }
-  //     },
-  //     {
-  //       // In the viewport
-  //       root: null,
-  //       threshold: 0,
-  //       rootMargin: "-200px",
-  //     }
-  //   );
-  //   obs.observe(headerEl);
-  // });
+        if (ent.isIntersecting === true) {
+          console.log("normal");
+          navEl.style.backgroundColor = null;
+        }
+      },
+      {
+        // In the viewport
+        root: null,
+        threshold: 0,
+        rootMargin: "-200px",
+      }
+    );
+    obs.observe(headerEl);
+  });
 
   return (
     <div className="app">
